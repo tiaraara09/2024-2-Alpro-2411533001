0203f1d34028e5dd6bb23a9989e505ef3178790c
ArA🦈 <ArA@UiiAra>
2024-09-19 10:53:54 +0700

    Pratikum Pekan 1

---
+++ Pratikum_Alpro_2_2411533001/src/Pekan1/HelloWorld.java
@@ -0,0 +1,24 @@
+package Pekan1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// program pertama
+		/* komentar
+		 * dua baris atau lebih
+		 * pertemuan 1 pratikum alpro
+		 */
+	System.out.println("Hello World");
+	System.out.println();
+	System.out.println("Program ini menghasilkan");
+	System.out.println("Empat Baris Output");
+	System.out.println();
+	System.out.println("/ \\ // \\\\ /// \\\\");
+	System.out.println();
+	System.out.println("This"+" program print a\n"
+	        + "quote from the Gettysburg Addres.");
+	} 
+	
+	
+
+}
